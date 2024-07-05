8360c2e062f52dd7a27f61ef20dbb617a111a7c7
Gokula krishnan R <105733059+gokula-krishnan-r-dev@users.noreply.github.com>
2024-07-05 22:37:19 +0530

    refactor: update axios base URL to use HTTPS

--- lib/axios.ts
+++ lib/axios.ts
@@ -5,7 +5,7 @@ const isServer = typeof window === 'undefined';
 export const accessToken = isServer ? '' : localStorage.getItem('token');
 // Immediately-invoked function to determine if we are server-side
 const axios = Axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://api.zynoflixott.com/api',
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${accessToken}`
